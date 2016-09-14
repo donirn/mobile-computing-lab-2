eabe21fbd3551bc91ef02479e357ddd74d962817
Doni Ramadhan <doniramadhan@gmail.com>
2016-09-14 11:04:53 +0200

    create table view controller to perform search book

---
+++ Lab2/SearchBookTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SearchBookTableViewController.swift
+//  Lab2
+//
+//  Created by Doni Ramadhan on 14/09/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class SearchBookTableViewController: UITableViewController {
+}
+
+extension SearchBookTableViewController{
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
