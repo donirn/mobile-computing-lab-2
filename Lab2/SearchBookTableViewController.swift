a245aeada113eb31dfd48e2c810cacb0558919ac
Doni Ramadhan <doniramadhan@gmail.com>
2016-09-16 12:10:54 +0200

    search bar request to openlibrary API

--- Lab2/SearchBookTableViewController.swift
+++ Lab2/SearchBookTableViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class SearchBookTableViewController: UITableViewController {
     let searchController = UISearchController(searchResultsController: nil)
     
+    let defaultSession = NSURLSession(configuration: .defaultSessionConfiguration())
+    var dataTask: NSURLSessionDataTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchController.dimsBackgroundDuringPresentation = false
@@ -33,6 +36,32 @@ extension SearchBookTableViewController{
 
 extension SearchBookTableViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-        print("search pressed: \(searchBar.text)")
+        guard let text = searchBar.text else {return}
+        let expectedCharSet = NSCharacterSet.URLQueryAllowedCharacterSet()
+        guard let searchTerm = text.stringByAddingPercentEncodingWithAllowedCharacters(expectedCharSet) else {return}
+        guard let url = NSURL(string: "https://openlibrary.org/search.json?q=" + searchTerm) else {return}
+        
+        dataTask?.cancel()
+        dataTask = defaultSession.dataTaskWithURL(url){
+            data, response, error in
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            }
+            
+            guard error == nil else { print(error!.localizedDescription); return}
+            guard (response as? NSHTTPURLResponse)?.statusCode == 200 else {return}
+            
+            if let data = data{
+                self.updateSearchResults(data)
+            }
+        }
+        
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        dataTask?.resume()
+    }
+    
+    func updateSearchResults(data: NSData){
+        print(data)
     }
 }
